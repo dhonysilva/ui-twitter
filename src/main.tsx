df5362282323fe6805dfcec245484f6da8c15f9d
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-02-20 00:02:04 -0300

    Adiciona estilização ao Sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,33 @@ import ReactDOM from 'react-dom/client'
 
 import './global.css'
 
+import twitterLogo from './assets/logo-twitter.svg'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    
+    <div className='layout'>
+      <aside className='sidebar'>
+        <img className='logo' src={twitterLogo} alt='Logo'></img>
+        
+        <nav className='main-navigation'>
+          <a href=''>Home</a>
+          <a href=''>Explore</a>
+          <a href=''>Notifications</a>
+          <a href=''>Messages</a>
+          <a href=''>Bookmarks</a>
+          <a href=''>Lists</a>
+          <a href=''>Profile</a>
+          <a href=''>More</a>
+        </nav>
+        
+        <button className='new-tweet' type='button'>
+          Tweet
+        </button>
+      </aside>
+      <div className='content'>
+        content
+      </div>
+    </div>
   </React.StrictMode>
 )
