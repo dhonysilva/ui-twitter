484af54a50a362d1261ecc6ed325cbdcc03b1cd0
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-02-19 23:25:00 -0300

    Adiciona mais estilizaçoes ao global.css e uma estilização inline

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -4,9 +4,13 @@ interface TweetProps {
     likes?: number
 }
 
+const tweetStyles = {
+    borderRadius: 8
+}
+
 export function Tweet(props: TweetProps) {
     return(
-        <div>
+        <div className="tweet" style={tweetStyles}>
             <strong>{props.user}</strong>
             <p>{props.children}</p>
             <button>Like {props.likes ?? 0}</button>
